d97722ef1338ad67cfbf8484fa829b6f9b8e20a2
USERSAD\EGENGIR3D <robert.gengiu@emeal.nttdata.com>
2024-10-26 22:11:56 +0300

    added more forecast infos // forecast for current day

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,24 @@
 cityInputField = document.getElementById("city-input");
+getWeatherBtn = document.getElementById('get-weather');
 
 cityInputField.addEventListener("input", function (e) {
     this.value = this.value.replace(/[^A-Za-z\s]/g, "");
 });
+
+myAPIkey = '9112df7df93eeef6138f52c4da4f36d3';
+
+getWeatherBtn.addEventListener('click', () => {
+
+    const city = cityInputField.value;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myAPIkey}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('temperature').textContent = `Temperature: ${data.main.temp} °C`;
+            document.getElementById('description').textContent = `Description: ${data.weather[0].description}`;
+            document.getElementById('humidity').textContent = `Humidity: ${data.main.humidity}%`;
+            document.getElementById('wind-speed').textContent = `Wind Speed: ${data.wind.speed} m/s`;
+            document.getElementById('pressure').textContent = `Pressure: ${data.main.pressure} hPa`; 
+        })
+        .catch(error => console.error('Error fetching current weather:', error));
+})
